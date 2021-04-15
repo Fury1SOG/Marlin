2689a4eb8bde2365384a83b00c7b6150c0db6fd1
Fury1SOG <tom.sog@gmail.com>
2021-04-15 13:46:31 +0900

    Update Configuration.h
    * Reverted #define X_MAX_POS X_BED_SIZE to default to avoid cable chain mount from hitting Z-frame.

--- Configuration Files/E3V2 Templates/UBL-BLTouch-10x10/Configuration.h
+++ Configuration Files/E3V2 Templates/UBL-BLTouch-10x10/Configuration.h
@@ -1240,7 +1240,7 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE + 15 // Extended max to allow the probe to reach more of the bed.
+#define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 250
 
@@ -1482,7 +1482,7 @@
   //#define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
 
   #define MESH_INSET 10              // Set Mesh bounds as an inset region of the bed
-  #define GRID_MAX_POINTS_X 10      // Don't use more than 15 points per axis, implementation limited.
+  #define GRID_MAX_POINTS_X 15      // Don't use more than 15 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   //#define UBL_HILBERT_CURVE       // Use Hilbert distribution for less travel when probing multiple points
