494a73135013b40f3714b36664d229063ce0aed3
Fury1SOG <tom.sog@gmail.com>
2021-04-17 14:34:06 +0900

    Updated PID values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -512,9 +512,9 @@
     #define DEFAULT_Kd_LIST {  78.81,  78.81 }
   #else
     // Ender 3 v2
-    #define DEFAULT_Kp  28.72
-    #define DEFAULT_Ki   2.62
-    #define DEFAULT_Kd  78.81
+    #define DEFAULT_Kp  16.20
+    #define DEFAULT_Ki  1.08
+    #define DEFAULT_Kd  60.69
   #endif
 #endif // PIDTEMP
 
@@ -552,9 +552,9 @@
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
   // Ender 3 V2
-  #define DEFAULT_bedKp 462.10
-  #define DEFAULT_bedKi  85.47
-  #define DEFAULT_bedKd 624.59
+  #define DEFAULT_bedKp 26.48
+  #define DEFAULT_bedKi 2.13
+  #define DEFAULT_bedKd 219.34
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
