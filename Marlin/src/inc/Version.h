6ef4b216088e9a8cfc91a177136c6708af9a3713
Fury1SOG <tom.sog@gmail.com>
2021-05-02 10:06:27 +0900

    Changed build version

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "E3V2-Extensible-UI"
+  #define SHORT_BUILD_VERSION "E3V2-Extensible-UI(ts_custom)"
 #endif
 
 /**
@@ -42,7 +42,7 @@
  * version was tagged.
  */
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2021-04-19"
+  #define STRING_DISTRIBUTION_DATE "2021-05-02"
 #endif
 
 /**
